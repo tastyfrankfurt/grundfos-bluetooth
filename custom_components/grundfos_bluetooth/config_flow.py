eae9d9c6870a7531e730894bded8a515f296a241
tastyfrankfurt <frednollows@gmail.com>
2025-12-13 13:44:18 +1000

    chore: removed bad function on startup.

--- custom_components/grundfos_bluetooth/config_flow.py
+++ custom_components/grundfos_bluetooth/config_flow.py
@@ -159,10 +159,6 @@ class GrundfosConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for Grundfos Bluetooth."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
